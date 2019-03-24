c288c491e1dbb4cc72be8b4ab826796919866df2
cosmos-sajal <sajal.4591@gmail.com>
2019-03-24 13:38:12 +0530

    change travis.exs

--- config/travis.exs
+++ config/travis.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :discuss, Discuss.Endpoint,
-  http: [port: 4001],
+  http: [port: 4000],
   server: false
 
 # Print only warnings and errors during test
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "discuss_test",
   hostname: "localhost",
   pool_size: 10
