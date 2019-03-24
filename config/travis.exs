03b5c88b77ba74d7219ea24259613c165d6ad957
cosmos-sajal <sajal.4591@gmail.com>
2019-03-24 13:26:31 +0530

    trying again for CI

--- config/travis.exs
+++ config/travis.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "",
+  password: "postgres",
   database: "discuss_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
